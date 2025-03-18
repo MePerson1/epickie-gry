e34f30e19faaeeb786a40901042044244fcbfe92
MePerson1 <112072726+MePerson1@users.noreply.github.com>
2025-03-18 18:13:16 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import NavigationBar from "./components/NavigationBar";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <>
-      <div>Witaj swiecie!!!</div>
+      <body>
+        <NavigationBar />
+      </body>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/pages/GamesPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const GamesPage = () => {
+  return <>Is it working?</>;
+};
