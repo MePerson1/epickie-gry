12900940cc8f57bcd89319257621e225e58116f0
MePerson1 <112072726+MePerson1@users.noreply.github.com>
2025-03-19 19:43:50 +0100

    edited themes and color pallet
    The new theme was created in index.css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="navbar bg-base-100 shadow-sm">
+    <nav className="navbar bg-secondary shadow-sm">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
