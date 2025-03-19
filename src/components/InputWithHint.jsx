228cc7553c42ceaf063d26b5f2b6fdb5b170c235
MePerson1 <112072726+MePerson1@users.noreply.github.com>
2025-03-19 21:30:30 +0100

    Created input component

---
+++ src/components/InputWithHint.jsx
@@ -0,0 +1,18 @@
+const InputWithHint = ({ fieldName, typeOf, example, hint }) => {
+  return (
+    <div>
+      <fieldset class="fieldset">
+        <legend class="fieldset-legend">{fieldName}</legend>
+        <input
+          class="input validator"
+          type={typeOf}
+          required
+          placeholder={example}
+        />
+        <div class="validator-hint">{hint}</div>
+      </fieldset>
+    </div>
+  );
+};
+
+export default InputWithHint;
--- src/views/LoginRegisterPage.jsx
+++ src/views/LoginRegisterPage.jsx
@@ -1,5 +1,22 @@
+import InputWithHint from "../components/InputWithHint";
+
 const LoginRegisterPage = () => {
-  return <>Login Register </>;
+  return (
+    <div className="flex justify-center">
+      <div className="grid grid-cols-2 gap-3">
+        <div className="border">
+          {" "}
+          <InputWithHint
+            fieldName="Email"
+            typeOf="email"
+            example="email@site.com"
+            hint="Podano nie poprawny email!"
+          />
+        </div>
+        <div className="border">Register</div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginRegisterPage;
