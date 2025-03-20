755c684dd200bf56aea1cde56fa8536f07e840f8
Paweł Tarkiewicz <112072726+MePerson1@users.noreply.github.com>
2025-03-20 16:29:41 +0100

    First view of login and register form

--- src/views/LoginRegisterPage.jsx
+++ src/views/LoginRegisterPage.jsx
@@ -2,18 +2,66 @@ import InputWithHint from "../components/InputWithHint";
 
 const LoginRegisterPage = () => {
   return (
-    <div className="flex justify-center">
-      <div className="grid grid-cols-2 gap-3">
-        <div className="border">
-          {" "}
+    <div className="flex justify-center items-center min-h-screen ">
+      <div className="flex flex-col md:flex-row items-center gap-8  p-8 shadow-lg rounded-2xl">
+        
+        {/* Login Form */}
+        <div className="bg-base-200 border border-primary rounded-2xl p-6 w-80 text-center">
+          <h2 className="text-2xl font-bold mb-4">Zaloguj się</h2>
+          <InputWithHint
+            fieldName="Login / Email"
+            typeOf="text"
+            example=""
+            hint="Podano nie poprawny login lub email!"
+          />
+          <InputWithHint
+            fieldName="Hasło"
+            typeOf="password"
+            example=""
+            hint="Haslo nie moze byc puste!"
+          />
+          <label className="flex items-center justify-center gap-2 text-sm mt-2">
+            <input type="checkbox" className="checkbox" />
+            Zapamiętaj mnie
+          </label>
+          <div className="text-sm text-blue-500 mt-2 cursor-pointer">
+            Nie pamiętasz hasła?
+          </div>
+          <button className="mt-4 w-full py-2 bg-primary text-white rounded-lg">Zaloguj</button>
+        </div>
+
+        {/* OR Divider */}
+        <div className="text-xl font-bold text-gray-500">Lub</div>
+
+        {/* Register Form */}
+        <div className="bg-base-200 border border-primary rounded-2xl p-6 w-80 text-center">
+          <h2 className="text-2xl font-bold mb-4">Zarejestruj się</h2>
           <InputWithHint
             fieldName="Email"
             typeOf="email"
-            example="email@site.com"
+            example="kowalski@email.com"
             hint="Podano nie poprawny email!"
           />
+          <InputWithHint
+            fieldName="Nazwa użytkownika"
+            typeOf="text"
+            example="Marian123"
+            hint="Nazwa użytkownika nie moze byc pusta!"
+          />
+          <InputWithHint
+            fieldName="Hasło"
+            typeOf="password"
+            example=""
+            hint="Haslo nie moze byc puste!"
+          />
+          <InputWithHint
+            fieldName="Powtórz hasło"
+            typeOf="password"
+            example=""
+            hint="Haslo nie moze byc puste!"
+          />
+          <button className="mt-4 w-full py-2 bg-primary text-white rounded-lg">Zarejestruj</button>
         </div>
-        <div className="border">Register</div>
       </div>
     </div>
   );
