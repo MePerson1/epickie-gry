62a79af04b3e5f829206638b4cc685572ac66125
Paweł Tarkiewicz <112072726+MePerson1@users.noreply.github.com>
2025-03-22 21:09:29 +0100

    color changes in index.css

--- src/components/NewsPage/NewsCardSmall.jsx
+++ src/components/NewsPage/NewsCardSmall.jsx
@@ -1,20 +1,21 @@
 import newsImage from '../../assets/newsImage.png'
 const NewsCardSmall = () =>
     {
-    
+        const loremIpsum = "Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60.";
         return(
-        <div className="flex flex-row gap-5 border-t-3 border-primary p-3">
+        <div className="grid grid-flow-col gap-5 border-t-3 border-primary p-3 items-start">
             
             <div>
                 <img className="w-50" alt="news-png" src={newsImage}/>
             </div>
-            <div className="grid grid-cols-1">
+            <div >
                 <div className="text-2xl font-bold">Aktualizacja skakania</div>
                 <a className='text-sm'>21.03.2025</a>
-                <div className="mt-3">Jakis opis opis opis opis opis opis opis opis opis opis opis</div>
-                <a className='link link-hover text-sm text-accent'>Czytaj dalej</a>
+                <div className="mt-3">{loremIpsum}</div>
+                <a className='link link-hover text-sm text-info'>Czytaj dalej</a>
             </div>
-        </div>);
+        </div>
+        );
     }
     
     export default NewsCardSmall
